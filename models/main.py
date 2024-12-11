a4227441f1cf98cd1766314c0ae3d5a3c578a78d
Stephen Pasch <ssp2188@columbia.edu>
2024-12-10 20:30:39 -0500

    added argument orginization of each step, just placeholders for rn

--- models/main.py
+++ models/main.py
@@ -1,12 +1,23 @@
 # main.py
+import argparse
 from data_preparation import DataPreparation
 from model_trainer import ModelTrainer
-from model_pruner import ModelPruner
 from model_quantizer import ModelQuantizer
+from model_distiller import ModelDistiller
 from model_evaluator import ModelEvaluator
 
 
 def main():
+    # parse arguments 
+    parser = argparse.ArgumentParser(description="Run Specific Steps of the Model Training Pipeline")
+    parser.add_argument("--train", action="store_true", help="Run model training step.")
+    parser.add_argument("--ptq", action="store_true", help="Run Post-Training Quantization (PTQ).")
+    parser.add_argument("--qat", action="store_true", help="Run Quantization Aware Training (QAT).")
+    parser.add_argument("--distill", action="store_true", help="Run Knowledge Distillation.")
+    parser.add_argument("--evaluate", action="store_true", help="Run model evaluation step.")
+    
+    args = parser.parse_args()
+    
     # general
     model_name = "meta-llama/Llama-3.2-1B"
     huggingface_token = "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"
@@ -37,40 +48,55 @@ def main():
     )
     dataset = data_preparation.prepare_data()
 
-    # train model
-    model_trainer = ModelTrainer(
-        model_name=model_name,
-        saved_data_path=saved_data_path,
-        model_results_path=model_results_path,
-        num_labels=num_labels,
-        epochs=epochs,
-        batch_size=batch_size,
-        learning_rate=learning_rate,
-        wandb_project=wandb_project,
-    )
-    
-    #Step 1: Finetune model
-    model_trainer.train_model()
+    if args.train:
+        #Step 1: Finetune model
+        model_trainer = ModelTrainer(
+            model_name=model_name,
+            saved_data_path=saved_data_path,
+            model_results_path=model_results_path,
+            num_labels=num_labels,
+            epochs=epochs,
+            batch_size=batch_size,
+            learning_rate=learning_rate,
+            wandb_project=wandb_project,
+        )
+        model_trainer.train_model()
 
-    # Step 2: Model Quantization (Destruction)
-    #PTQ
-    model_quantizer = ModelQuantizer()
-    model_quantizer.ptq()
-    # model_quantizer.quantize_model()
-    model_quantizer.save_model()
+    if args.ptq or args.distill:
+        # Step 2: Model Quantization (Destruction) #PTQ
+        model_quantizer = ModelQuantizer()
+        model_quantizer.ptq()
+        model_quantizer.save_model()
     
-    #Step 3.1 QAT (Recovery mthd. 1)
-    # model_trainer.qat()
+    if args.qat:
+        #Step 3.1 QAT (Recovery mthd. 1)
+        model_trainer = ModelTrainer(
+            model_name=model_name,
+            saved_data_path=saved_data_path,
+            model_results_path=model_results_path,
+            num_labels=num_labels,
+            epochs=epochs,
+            batch_size=batch_size,
+            learning_rate=learning_rate,
+            wandb_project=wandb_project,
+        )
+        model_trainer.qat()
     
-    #Step 3.2 Knowledge Distialation (Recovery mthd. 2)
-    # model_distiller = ModelDistiller(teacher_model, model_quantizer)
-    # model_distiller.train_knowledge_distillation(train_loader, epochs, learning_rate, T, soft_target_loss_weight, ce_loss_weight)
-    # model_distiller.save_model() # optionally provide save_path
+    if args.distill:
+        # Step 3.2 Knowledge Distialation (Recovery mthd. 2)
+        teacher_model = None # placeholder
+        model_distiller = ModelDistiller(teacher_model, model_quantizer)
+        train_loader = None # placeholder
+        T = 1.0 # placeholder
+        soft_target_loss_weight = 0.5 # placeholder
+        ce_loss_weight = 0.5 # placeholder
+        model_distiller.train_knowledge_distillation(train_loader, epochs, learning_rate, T, soft_target_loss_weight, ce_loss_weight)
+        model_distiller.save_model() # optionally provide save_path
 
-    # # Step 4: Model Evaluation
-    # model_evaluator = ModelEvaluator()
-    # model_evaluator.evaluate()
-
+    if args.evaluate:
+        # Step 4: Model Evaluation
+        model_evaluator = ModelEvaluator()
+        model_evaluator.evaluate()
 
 if __name__ == "__main__":
     main()
