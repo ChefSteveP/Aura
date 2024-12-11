f38f36f519441a0efc9fac69dfdf2f4b47da628c
Stephen Pasch <ssp2188@columbia.edu>
2024-12-10 20:11:18 -0500

    update device reference

--- models/model_distiller.py
+++ models/model_distiller.py
@@ -21,8 +21,8 @@ class ModelDistiller:
             raise ValueError("Both teacher and student models must be provided.")
         
         self.device = "cuda" if torch.cuda.is_available() else "cpu"
-        self.teacher_model = teacher.to(device)
-        self.student_model = student.to(device)
+        self.teacher_model = teacher.to(self.device)
+        self.student_model = student.to(self.device)
         
     def train_knowledge_distillation(self, train_loader, epochs, 
                                      learning_rate, T, soft_target_loss_weight, 
